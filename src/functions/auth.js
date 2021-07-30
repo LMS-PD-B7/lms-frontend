c1fbec519f6d07a6113caba99cf2e506a3eb6ff1
metardn <54663326+metardn@users.noreply.github.com>
2021-07-30 21:49:44 +0700

    update Login and Register

--- src/functions/auth.js
+++ src/functions/auth.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { getAuthHeader } from './authHeader';
 
 export const login = (params) => {
     axios.post(process.env.REACT_APP_API_URL + "/auth/login", params).then((res) => {
@@ -6,6 +7,16 @@ export const login = (params) => {
 
         sessionStorage.setItem("user_token", res.data.token);
         window.location.reload();
+    }).catch((err) => {
+        console.error(err);
+    })
+}
+
+export const register = (params) => {
+    axios.post(process.env.REACT_APP_API_URL + "/auth/register", params).then((res) => {
+        console.info(res);
+
+        login(params);
     }).catch((err) => {
         console.error(err);
     })
